d18f4f696f481dc671e301f47be006717b20bff5
Bartektrr <109850860+Bartektrr@users.noreply.github.com>
2022-12-26 09:28:42 +0100

    Revert "Fix typo in the username"
    This reverts commit 83f2597bb64fe22e105e0f3be4a601b2de2380f8.

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,7 @@ passport.use(new LocalStrategy(function verify(username, password, cb) {
 
 passport.serializeUser(function(user, cb) {
   process.nextTick(function() {
-    cb(null, { id: user.id, username: user.Username, role: user.Role });
+    cb(null, { id: user.id, username: user.username });
   });
 });
 
@@ -35,8 +35,7 @@ passport.deserializeUser(function(user, cb) {
 
 var router = express.Router();
 router.get('/login', function(req, res, next) {
-  const username = req.user?.username;
-  res.render('login', { username });
+  res.render('login');
 });
 router.post('/login/password', passport.authenticate('local', {
   successReturnToOrRedirect: '/',
